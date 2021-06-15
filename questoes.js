18102ccd9a2e7fd6011a9d72ed2949f07f8e3717
tutu202 <79462020+tutu202@users.noreply.github.com>
2021-06-15 15:09:28 -0300

    Arquivo com as questoes
    oieee

---
+++ questoes.js
@@ -0,0 +1,23 @@
+function dizOla(text) {
+return 'Olá, ${text}!'
+}
+
+function operador(num1, num2, callback) {
+console.log(callback(num1, num2))
+}
+
+function verificaDivisibilidade(num, callback) {
+const callbackNumber = callback( )
+
+return num % callbackNumber = 0
+}
+
+function mesNumParaString(num) {
+const months = ['Janeiro','Fevereiro','Março','Abril','Maio','junho','Agosto','Setembro','Outubro','Novembro','Dezembro']
+
+return months[num - 1]
+}
+
+function verificaMaiorOuIgual(n1, n2, callback) {
+return callback(n1, n2)
+}
